d72f1d3442bb74ded2b358d826af722fca1ae30d
Rafał Ostrowski <oester200729@gmail.com>
2018-07-16 21:49:33 +0200

    Compare arrays in assert + immutable array reverse

--- js.js
+++ js.js
@@ -1,9 +1,21 @@
 // Simplest assert possible for quokka prototyping
 
-const should = (expected, actual) => {
-  if (expected == actual) {
+const should = (actual, expected) => {
+  if (actual && expected && actual.length === expected.length
+    && actual.every && actual.every((v, i) => v === expected[i])) {
+      return 'OK'
+    }
+
+  if (expected === actual) {
     return 'OK'
   } else {
     return `Expected ${expected} but got ${actual}`
   }
 }
+
+// Immutably reverse an array
+
+const immutableReverse = (string) => {
+  return splitString(string).reverse().join('')
+}
+
